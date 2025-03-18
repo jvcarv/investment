9948a737ff1bc356891ebf087b81b754d2219743
João <jcarv.contato@gmail.com>
2025-03-18 14:44:27 -0300

    feat: the app component is now listening the event from the user-input and dynamically displaying the investment component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'investment';
+  calculate? :boolean;
+
+
+  onCalculate(){
+    this.calculate = true;
+  }
 }

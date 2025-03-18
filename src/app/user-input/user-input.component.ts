8064e296ced0427f8d7b1ea6b6abd66d2f076083
João <jcarv.contato@gmail.com>
2025-03-18 14:39:05 -0300

    feat: the user's component is done       it contains a model that manipulates a form and emits an event called 'calculate' which says to the program when the user wants to calculate his investiments

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+    input: number = 0;
+    initialInvestment: number = 0;
+    annualInvestment: number = 0;
+    expectedReturn: number = 0;
+    duration: number = 0;
+    isCalculated:boolean = false;
+    @Output() calculate = new EventEmitter<void>();
 
+    onCalculate(){
+      console.log("hehe");
+      this.calculate.emit();
+    }
 }
